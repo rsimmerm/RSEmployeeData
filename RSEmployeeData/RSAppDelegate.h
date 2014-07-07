cd88f00906ee0cac1aef33eb80073e2b4d20ed88
Ross Simmerman <rosssimmerman@Ross-Laptop.local>
2014-07-07 15:12:24 -0400

    Initial Commit

---
+++ RSEmployeeData/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  RSEmployeeData
+//
+//  Created by Ross Simmerman on 7/7/14.
+//  Copyright (c) 2014 RossSimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RSEmployeeDataTests/RSEmployeeDataTests.m
@@ -0,0 +1,34 @@
+//
+//  RSEmployeeDataTests.m
+//  RSEmployeeDataTests
+//
+//  Created by Ross Simmerman on 7/7/14.
+//  Copyright (c) 2014 RossSimmerman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RSEmployeeDataTests : XCTestCase
+
+@end
+
+@implementation RSEmployeeDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
